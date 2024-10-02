52543bd2505cc8337625bd8cd8bdb0505f08f005
hlina alemayoh <hilubecoder@gmail.com>
2024-10-02 08:34:00 +0300

    completly change it

--- quiz.js
+++ quiz.js
@@ -1,22 +1,26 @@
 
-  function checkAnswer(){
-    const correctAnswer ="4";
-    const ischecked= document.querySelector('input[name="quiz"]:checked');
-    if(ischecked){
-   const userAnswer= ischecked.value ;
-  
-   if(userAnswer === correctAnswer){
-    document.getElementById('feedback').textContent ="Correct! Well done.";
-   }
-    else{
-        document.getElementById('feedback').textContent="That's incorrect. Try again!";
-    }
-    }
-else {
+  function checkAnswer() {
+   
+    const correctAnswer = '4';
+
+   
+    const ischecked = document.querySelector('input[name="quiz"]:checked');
+
+    
+    if (ischecked) {
+        const userAnswer = ischecked.value;
+
+       
+        if (userAnswer === correctAnswer) { 
+            document.getElementById('feedback').textContent = "Correct! Well done.";
+        } else {
+            document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+        }
+    } else {
+        
         document.getElementById('feedback').textContent = "Please select an answer.";
     }
+}
 
-  }
-
-   const submit = document.getElementById("submit-answer");
-  submit.addEventListener('click', checkAnswer);
+const submit = document.getElementById("submit-answer");
+submit.addEventListener('click', checkAnswer);
