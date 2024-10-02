397dc12a39f3d95d4650c4b0ee9bfd4eb15a88ef
hlina alemayoh <hilubecoder@gmail.com>
2024-10-02 08:29:35 +0300

    add some corrections

--- quiz.js
+++ quiz.js
@@ -1,15 +1,21 @@
 
   function checkAnswer(){
-    const correctAnswer ='4';
+    const correctAnswer ="4";
     const ischecked= document.querySelector('input[name="quiz"]:checked');
+    if(ischecked){
    const userAnswer= ischecked.value ;
-
+  
    if(userAnswer === correctAnswer){
     document.getElementById('feedback').textContent ="Correct! Well done.";
    }
     else{
         document.getElementById('feedback').textContent="That's incorrect. Try again!";
     }
+    }
+else {
+        document.getElementById('feedback').textContent = "Please select an answer.";
+    }
+
   }
 
    const submit = document.getElementById("submit-answer");
